20ea76b4e6c93ada285af3cf76e969de10c8e4f4
kacperchm <chmuraka1998@gmail.com>
2023-06-26 22:52:46 +0200

    Created new methods in BooksRepository and UsersRepository

--- library-backend/src/main/java/com/kacperchm/librarybackend/repository/BooksRepository.java
+++ library-backend/src/main/java/com/kacperchm/librarybackend/repository/BooksRepository.java
@@ -2,8 +2,27 @@ package com.kacperchm.librarybackend.repository;
 
 import com.kacperchm.librarybackend.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BooksRepository extends JpaRepository<Book, Long> {
+    List<Book> findAllByAuthorContains(String author);
+    List<Book> findAllByTitleContains(String title);
+    List<Book> findAllByCategory(String category);
+
+    @Query("SELECT b FROM Book b WHERE b.category = :category AND b.title LIKE %:title% AND b.author LIKE %:author%")
+    List<Book> findBooksByCategoryTitleAndAuthor(@Param("category") String category, @Param("title") String title, @Param("author") String author);
+
+    @Query("SELECT b FROM Book b WHERE b.category = :category AND b.title LIKE %:title%")
+    List<Book> findBooksByCategoryAndTitle(@Param("category") String category, @Param("title") String title);
+
+    @Query("SELECT b FROM Book b WHERE b.category = :category AND b.author LIKE %:author%")
+    List<Book> findBooksByCategoryAndAuthor(@Param("category") String category, @Param("author") String author);
+
+    @Query("SELECT b FROM Book b WHERE b.title LIKE %:title% AND b.author LIKE %:author%")
+    List<Book> findBooksByTitleAndAuthor(@Param("title") String title, @Param("author") String author);
 }
--- library-backend/src/main/java/com/kacperchm/librarybackend/repository/UsersRepository.java
+++ library-backend/src/main/java/com/kacperchm/librarybackend/repository/UsersRepository.java
@@ -11,5 +11,7 @@ public interface UsersRepository extends JpaRepository<User,Long> {
     List<User> findByUsername(String username);
     List<User> findByMail(String mail);
     boolean existsByUsername(String username);
+    boolean existsByMail(String mail);
+    boolean existsByPhoneNumber(String mail);
 
 }
