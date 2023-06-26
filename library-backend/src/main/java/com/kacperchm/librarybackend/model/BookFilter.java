81c9ae85acb0da63be349339b2247f4434e44a11
kacperchm <chmuraka1998@gmail.com>
2023-06-26 22:59:20 +0200

    BookFilter created to simplify the transfer of information about searched books

---
+++ library-backend/src/main/java/com/kacperchm/librarybackend/model/BookFilter.java
@@ -0,0 +1,37 @@
+package com.kacperchm.librarybackend.model;
+
+public class BookFilter {
+    private String category;
+    private String author;
+    private String title;
+
+    public BookFilter(String category, String author, String title) {
+        this.category = category;
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
