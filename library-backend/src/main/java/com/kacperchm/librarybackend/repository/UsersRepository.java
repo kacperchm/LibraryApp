3bf55a9247363f2aabfccdadd989c91cc5ecb374
kacperchm <chmuraka1998@gmail.com>
2023-07-02 23:26:59 +0200

    Add method 'findUsersByUsernamePhoneNumberAndMail' in UsersRepository

--- library-backend/src/main/java/com/kacperchm/librarybackend/repository/UsersRepository.java
+++ library-backend/src/main/java/com/kacperchm/librarybackend/repository/UsersRepository.java
@@ -1,7 +1,10 @@
 package com.kacperchm.librarybackend.repository;
 
+import com.kacperchm.librarybackend.model.Book;
 import com.kacperchm.librarybackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +17,6 @@ public interface UsersRepository extends JpaRepository<User,Long> {
     boolean existsByMail(String mail);
     boolean existsByPhoneNumber(String mail);
 
+    @Query("SELECT u FROM User u WHERE u.username LIKE %:username% AND u.phoneNumber LIKE %:phoneNumber% AND u.mail LIKE %:mail%")
+    List<User> findUsersByUsernamePhoneNumberAndMail(@Param("username") String username, @Param("phoneNumber") String phoneNumber, @Param("mail") String mail);
 }
